77e897aa4279d4d968d6afc7c45e35f356d0e191
cokeefe0319 <cokeefe0319@stu.d214.org>
2018-10-31 09:20:59 -0500

    Initial Commit

---
+++ PHS Vehicle Check/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PHS Vehicle Check
+//
+//  Created by Timothy P. Konopacki on 10/29/18.
+//  Copyright © 2018 TK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
